cf9ca3383c6c5eb34e12dd1ba6b0251484824218
George <gramos@vibrenthealth.com>
2026-08-18 22:11:16 -0400

    Revert "Add Nevin pitching clinic booking"
    This reverts commit 994ed80defa6b23ed553b9117bedab26db9564b2.

--- app/nevins-pitching/layout.jsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Nevin's Pitching Clinic | Velo Performance Lab",
-  description: 'Wednesday and Thursday pitching clinics with former White Sox pitcher Nevin Griffith.',
-};
-
-export default function NevinsPitchingLayout({ children }) {
-  return children;
-}
